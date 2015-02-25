74b445a17d47dcda37a43c522df31ab057d188cf
Mark Riedesel <mark@klowner.com>
2015-02-25 10:56:16 -0600

    this works

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -1,25 +1,41 @@
+var async = require('async');
+
 module.exports = function (server) {
 
+	var ds = server.datasources.db; //postgres;
 
-	server.models.Device.create({
-		serial: 'potato',
-		name: 'Crapinator 3000'
-	}, function (err) {
-		if (err) { throw err; }
+	function createSampleModels(cb) {
 
-		server.models.Partner.create({
-			partnerid: 1,
-			name: 'Pete Crapmanigan'
+		server.models.PrefixedDevice.create({
+			serial: 'potato',
+			name: 'Crapinator 3000'
 		}, function (err) {
 			if (err) { throw err; }
 
-			server.models.Registry.create({
+			server.models.PrefixedPartner.create({
 				partnerid: 1,
-				serial: 'potato'
+				name: 'Pete Crapmanigan'
 			}, function (err) {
 				if (err) { throw err; }
 
+				server.models.PrefixedRegistry.create({
+					partnerid: 1,
+					serial: 'potato'
+				}, function (err) {
+					if (err) { throw err; }
+
+					cb();
+				});
 			});
 		});
+	}
+
+	var automigrate = function (cb) {
+		ds.automigrate(function () {
+			createSampleModels(cb);
+		});
+	};
+
+	async.series([automigrate], function (err, results) {
 	});
 }
